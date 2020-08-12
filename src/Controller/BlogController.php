3bff8248ad9f2bab4e6f3d2dd88be02f7251bcdd
Lwmay <45488236+Lwmay@users.noreply.github.com>
2020-08-12 09:41:55 +0400

    Initial commit.
    Create post entity.

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class BlogController
+ * @package App\Controller
+ */
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
+        return $this->render("index.html.twig", [
+            "posts" => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/article-{id}", name="blog_read")
+     * @param Post $post
+     * @return Response
+     */
+    public function read(Post $post): Response
+    {
+        return $this->render("read.html.twig", [
+            "post" => $post
+        ]);
+    }
+}
+
